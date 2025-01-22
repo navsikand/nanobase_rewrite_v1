6f051ad1357de76470a7ecf470515877dc9bd5d9
Navraj Sikand <nonunavraj@gmail.com>
2025-01-22 10:40:14 -0700

    Allowed images from the nanobase server

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "nanobase.org",
+        hostname: "api.nanobase.org",
         // port: '',
         // pathname: '',
       },
